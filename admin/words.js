961f3ff51e8523da2fca8e4deeb54266cf64fb2a
maxhro <maximilian.risch@gmx.net>
2024-08-02 15:24:31 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'ai-control adapter settings': {
+		en: 'Adapter settings for ai-control',
+		de: 'Adaptereinstellungen für ai-control',
+		ru: 'Настройки адаптера для ai-control',
+		pt: 'Configurações do adaptador para ai-control',
+		nl: 'Adapterinstellingen voor ai-control',
+		fr: "Paramètres d'adaptateur pour ai-control",
+		it: "Impostazioni dell'adattatore per ai-control",
+		es: 'Ajustes del adaptador para ai-control',
+		pl: 'Ustawienia adaptera dla ai-control',
+		uk: 'Налаштування адаптера для ai-control',
+		'zh-cn': 'ai-control的适配器设置',
+	},
+	option1: {
+		en: 'option1',
+		de: 'Option 1',
+		ru: 'Опция 1',
+		pt: 'Opção 1',
+		nl: 'Optie 1',
+		fr: 'Option 1',
+		it: 'opzione 1',
+		es: 'Opción 1',
+		pl: 'opcja 1',
+		uk: 'варіант1',
+		'zh-cn': '选项1',
+	},
+	option2: {
+		en: 'option2',
+		de: 'Option 2',
+		ru: 'вариант 2',
+		pt: 'opção 2',
+		nl: 'Optie 2',
+		fr: 'Option 2',
+		it: 'opzione 2',
+		es: 'opcion 2',
+		pl: 'Opcja 2',
+		uk: 'варіант2',
+		'zh-cn': '选项2',
+	},
+};
